f1c809ae3c8ed2fb86a63adba3991007c0a3acaa
Michal Karczewski <michal.karczewski@estimote.com>
2017-09-25 16:22:18 +0200

    Add gitignore and change runner script

--- src/runner.py
+++ src/runner.py
@@ -4,8 +4,8 @@ import rospy
 print "[Robotics Indoor SDK] initializing ROS node.."
 rospy.init_node('positioning', anonymous=True)
 print "[Robotics Indoor SDK] ROS node started, initializing positioning system.."
-import lib.armv7l.robotics_indoor_sdk
-#import lib.x86.robotics_indoor_sdk
+#from lib.armv7l.robotics_indoor_sdk import robotics_indoor_sdk
+from lib.x86.robotics_indoor_sdk import robotics_indoor_sdk
 
 while True:
     time.sleep(0.2)
