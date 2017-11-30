2386c4b17e86835ee612f1420115d4cc2e1d6c16
Grzegorz Krukiewicz-Gacek <greggscutum@gmail.com>
2017-11-30 16:11:27 +0100

    Fixed imports in runner.py and updated package.xml

--- src/runner.py
+++ src/runner.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 import time
-import sys
 import rospy
 print "[Robotics Indoor SDK] initializing ROS node.."
 rospy.init_node('positioning', anonymous=True)
 print "[Robotics Indoor SDK] ROS node started, initializing positioning system.."
-# sys.path.append('lib/armv7l')
-sys.path.append('lib/x86')
-import robotics_indoor_sdk
+
+#import lib.x86.robotics_indoor_sdk
+import lib.armv7l.robotics_indoor_sdk
     
 while True:
     time.sleep(0.2)
